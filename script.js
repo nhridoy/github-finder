91ff42adfda484cc825a375ba915d56ed3559d96
Mona Lisa <75487507+hridoyboss12@users.noreply.github.com>
2021-06-14 22:50:55 +0600

    bug fixed

--- script.js
+++ script.js
@@ -11,57 +11,17 @@ btn.addEventListener("click", (e) => {
     fetch(`https://api.github.com/users/${userName}`)
       .then((result) => result.json())
       .then((data) => {
-        let full_name = data.name;
-        let company = data.company;
-        let blog = data.blog;
-        let location = data.location;
-        let email = data.email;
-        let hireable = data.hireable;
-        let bio = data.bio;
-        let twitter_username = data.twitter_username;
-        let public_repos = data.public_repos;
-        let public_gists = data.public_gists;
-        let followers = data.followers;
-        let following = data.following;
-        let created_at = data.created_at;
-        let updated_at = data.updated_at;
-        let login = data.login;
-        let avatar_url = data.avatar_url;
-        let html_url = data.html_url;
-        let message = data.message;
-
         if (data.message == "Not Found") {
           let info = new Informations();
-
           info.notFound("User Not Found");
         } else {
-          let info = new Informations(
-            login,
-            avatar_url,
-            html_url,
-            full_name,
-            company,
-            blog,
-            location,
-            email,
-            hireable,
-            bio,
-            twitter_username,
-            public_repos,
-            public_gists,
-            followers,
-            following,
-            created_at,
-            updated_at
-          );
-
-          info.showInfo();
+          let info = new Informations();
+          info.showInfo(data);
         }
       });
   } else {
     let info = new Informations();
-
-    info.notFound("Enter UserName!!!");
+    info.clearProfile("Enter UserName!!!");
   }
   e.preventDefault();
 });
--- ui.js
+++ ui.js
@@ -1,106 +1,64 @@
-class Users {
-  constructor(login, avatar_url, html_url) {
-    this.login = login;
-    this.avatar_url = avatar_url;
-    this.html_url = html_url;
-  }
-}
-
-class Informations extends Users {
-  constructor(
-    login,
-    avatar_url,
-    html_url,
-    full_name,
-    company,
-    blog,
-    location,
-    email,
-    hireable,
-    bio,
-    twitter_username,
-    public_repos,
-    public_gists,
-    followers,
-    following,
-    created_at,
-    updated_at
-  ) {
-    super(login, avatar_url, html_url);
-
-    this.full_name = full_name;
-    this.company = company;
-    this.blog = blog;
-    this.location = location;
-    this.email = email;
-    this.hireable = hireable;
-    this.bio = bio;
-    this.twitter_username = twitter_username;
-    this.public_repos = public_repos;
-    this.public_gists = public_gists;
-    this.followers = followers;
-    this.following = following;
-    this.created_at = created_at;
-    this.updated_at = updated_at;
+class Informations {
+  constructor() {
+    this.div = document.querySelector(".profile");
   }
 
-  showInfo() {
-    let div = document.querySelector(".profile");
-    div.innerHTML = `
+  showInfo(data) {
+    this.div.innerHTML = `
         <div class="card">
                 <div class="row p-3">
                     <div class="col-md-4 border border-3 border-primary p-3 mb-sm-3">
-                        <img src="${this.avatar_url}" alt=""
+                        <img src="${data.avatar_url}" alt=""
                             class="img-fluid border border-3 border-primary mb-3">
                         <div class="row mb-3 p-3">
                             <div class="col-md-6 col-sm-6 border border-3 border-primary">
                                 <i class="bi bi-people-fill"></i>
-                                <span>Followers: <strong>${this.followers}</strong></span>
+                                <span>Followers: <strong>${data.followers}</strong></span>
                             </div>
                             <div class="col-md-6 col-sm-6 border border-3 border-primary">
                                 <i class="bi bi-people"></i>
-                                <span>Following: <strong>${this.following}</strong></span>
+                                <span>Following: <strong>${data.following}</strong></span>
                             </div>
                         </div>
 
-                        <a href="${this.html_url}" target="_blank" class="d-grid btn btn-success border border-3 border-primary">View in Github</a>
+                        <a href="${data.html_url}" target="_blank" class="d-grid btn btn-success border border-3 border-primary">View in Github</a>
                     </div>
                     <div class="col-md-8">
                         <div class="border border-3 border-primary d-flex align-items-center mb-1">
-                            <p class="m-2">Name: <strong>${this.full_name}</strong></p>
+                            <p class="m-2">Name: <strong>${data.full_name}</strong></p>
                         </div>
                         <div class="border border-3 border-primary d-flex align-items-center mb-1">
-                            <p class="m-2">Location: <strong>${this.location}</strong></p>
+                            <p class="m-2">Location: <strong>${data.location}</strong></p>
                         </div>
                         <div class="border border-3 border-primary d-flex align-items-center mb-1">
-                            <p class="m-2">Public Repos: <strong>${this.public_repos}</strong></p>
+                            <p class="m-2">Public Repos: <strong>${data.public_repos}</strong></p>
                         </div>
                         <div class="border border-3 border-primary d-flex align-items-center mb-1">
-                            <p class="m-2">Public Gists: <strong>${this.public_gists}</strong></p>
+                            <p class="m-2">Public Gists: <strong>${data.public_gists}</strong></p>
                         </div>
                         <div class="border border-3 border-primary d-flex align-items-center mb-1">
-                            <p class="m-2">Bio: <strong>${this.bio}</strong></p>
+                            <p class="m-2">Bio: <strong>${data.bio}</strong></p>
                         </div>
                         <div class="border border-3 border-primary d-flex align-items-center mb-1">
-                            <p class="m-2">Email: <strong>${this.email}</strong></p>
+                            <p class="m-2">Email: <strong>${data.email}</strong></p>
                         </div>
                         <div class="border border-3 border-primary d-flex align-items-center mb-1">
-                            <p class="m-2">Ready to Hire: <strong>${this.hireable}</strong></p>
+                            <p class="m-2">Ready to Hire: <strong>${data.hireable}</strong></p>
                         </div>
                         <div class="border border-3 border-primary d-flex align-items-center mb-1">
-                            <p class="m-2">Company: <strong>${this.company}</strong></p>
+                            <p class="m-2">Company: <strong>${data.company}</strong></p>
                         </div>
                         <div class="border border-3 border-primary d-flex align-items-center mb-1">
-                            <p class="m-2">Twitter: <strong>${this.twitter_username}</strong></p>
+                            <p class="m-2">Twitter: <strong>${data.twitter_username}</strong></p>
                         </div>
                         <div class="border border-3 border-primary d-flex align-items-center mb-1">
-                            <p class="m-2">Website: <strong>${this.blog}</strong></p>
+                            <p class="m-2">Website: <strong>${data.blog}</strong></p>
                         </div>
                         <div class="border border-3 border-primary d-flex align-items-center mb-1">
-                            <p class="m-2">Account Created: <strong>${this.created_at}</strong></p>
+                            <p class="m-2">Account Created: <strong>${data.created_at}</strong></p>
                         </div>
                         <div class="border border-3 border-primary d-flex align-items-center mb-1">
-                            <p class="m-2">Last Activity: <strong>${this.updated_at}</strong></p>
+                            <p class="m-2">Last Activity: <strong>${data.updated_at}</strong></p>
                         </div>
                     </div>
                 </div>
@@ -108,13 +66,13 @@ class Informations extends Users {
         `;
   }
 
+  clearProfile(message) {
+    this.div.innerHTML = "";
+    this.notFound(message);
+  }
   notFound(message) {
-    let div = document.querySelector(".profile");
-    div.innerHTML = `
+    this.div.innerHTML = `
         <div class = "d-grid d-flex align-items-center justify-content-center bg-danger p-2 link-light border border-3 border-primary">${message}</div>
         `;
-    setTimeout(() => {
-      div.remove();
-    }, 3000);
   }
 }
